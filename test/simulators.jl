48e0c10bdb8feb9552fd9b187d97f9ea3f80dc02
WhiffleFish <tylertronics@gmail.com>
2025-05-15 14:04:31 -0600

    `RolloutSimulator` smoke test

--- test/simulators.jl
+++ test/simulators.jl
@@ -2,8 +2,9 @@
     game = SimpleMG()
     sol = DummySolver()
     pol = solve(sol, game)
+    
+    ## History
     sim = HistoryRecorder(max_steps=10)
-
     hist = simulate(sim, game, pol)
     @test all(iszero, hist[:r])
     @test length(hist) == 10
@@ -13,4 +14,10 @@
     @test all(isone, hist[:s])
     @test all(==((1,1)), hist[:a])
     @test all(isone, hist[:sp])
+
+
+    ## Rollouts
+    sim = RolloutSimulator(max_steps=10)
+    ret = simulate(sim, game, pol, rand(initialstate(game)))
+    @assert ret isa Float64
 end
